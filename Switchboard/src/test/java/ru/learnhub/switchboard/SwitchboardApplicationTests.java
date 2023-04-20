0002afa9848522f1817c9116d40c1c3f056d5eb5
Kurdikov Artem <kourtema@gmail.com>
2023-04-20 18:23:58 +0300

    Initial commit

---
+++ Switchboard/src/test/java/ru/learnhub/switchboard/SwitchboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.learnhub.switchboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
